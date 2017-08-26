37e78420cf8d17423eadda529140e884c8f82f48
giggo1604 <giggo1604@gmail.com>
2017-08-27 00:34:26 +0200

    fixed tests

--- test/e2e/specs/Launch.spec.js
+++ test/e2e/specs/Launch.spec.js
@@ -4,8 +4,14 @@ describe('Launch', function () {
     beforeEach(utils.beforeEach);
     afterEach(utils.afterEach);
 
+    it('shows two initial windows', function () {
+        return this.app.client.getWindowCount().then(function (count) {
+            expect(count).to.equal(2);
+        });
+    });
+
     it('shows the proper application title', function () {
-        return this.app.client.getTitle()
+        return this.app.client.windowByIndex(0).getTitle()
             .then((title) => {
                 expect(title).to.equal('movescount-export');
             });
