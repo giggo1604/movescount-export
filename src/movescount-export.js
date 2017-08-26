ca82e7afada6537abb4f2586601a451d207b407f
giggo1604 <giggo1604@gmail.com>
2017-08-26 13:26:07 +0200

    added calculated dates

--- src/movescount-export.js
+++ src/movescount-export.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
+const moment = require('moment');
 
 const exportBaseURL = 'http://www.movescount.com/move/export?';
 const folder = './downloads/';
@@ -8,8 +9,8 @@ async function movescountExport([config, activityRecordsData, cookies]) {
     const {UserId} = config;
     const {token, activityRecordsBaseUrl: baseUrl} = activityRecordsData;
 
-    const startDateQuery = 'startDateString=2017-07-27T00:00:00.000Z';
-    const endDateQuery= 'endDateString=2017-08-25T23:59:59.999Z';
+    const startDateQuery = 'startDateString=' + moment.utc().subtract(100, 'days').startOf('day').toISOString();;
+    const endDateQuery= 'endDateString=' + moment.utc().endOf('day').toISOString();
     const userIdQuery = `userId=${UserId}`
 
     const url = `${baseUrl}/moves/getmoves/?${startDateQuery}&${endDateQuery}&${userIdQuery}`;
